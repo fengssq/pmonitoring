5b20050ccc8d5effe688c27b9b2feed65ab8ebe3
Gru-ht <softcs@163.com>
2016-07-04 14:54:00 +0800

    three ok

--- libs/rules.py
+++ libs/rules.py
@@ -1,6 +1,7 @@
 #coding:utf8
 #time:'上午10:46:16-7-4:2016'
 #author:Gru_GHT
+#info:information fifter rules
 
 import re,os,sys
 
---
+++ libs/sqlmapapp.py
@@ -0,0 +1,4 @@
+#coding:utf8
+#time:'下午2:50:16-7-4:2016'
+#author:Gru_GHT
+#info: using sql injection
